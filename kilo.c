e63e83791e73e46fea944316b3a5aeddfc220617
Friedrich Doku <friedrichdoku@gmail.com>
2017-12-27 08:07:19 -0600

    Added cursor control.

--- kilo.c
+++ kilo.c
@@ -26,6 +26,13 @@ struct abuf {
     int len;
 };
 
+enum editorKey {
+  ARROW_LEFT = 1000,
+  ARROW_RIGHT,
+  ARROW_UP,
+  ARROW_DOWN
+};
+
 #define ABUF_INIT {NULL, 0}
 
 void 
@@ -115,31 +122,74 @@ getWindowSize(int *rows, int *cols) {
   }
 }
 
-char 
+int 
 editorReadKey(){
     int nread;
     char c;
     while ((nread = read(STDIN_FILENO, &c, 1)) != 1){
         if (nread == -1 && errno != EAGAIN) err("read");
     }
+
+  if (c == '\x1b') {
+    char seq[3];
+    if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
+    if (seq[0] == '[') {
+      switch (seq[1]) {
+        case 'A': return ARROW_UP;
+        case 'B': return ARROW_DOWN;
+        case 'C': return ARROW_RIGHT;
+        case 'D': return ARROW_LEFT;
+      }
+    }
+    return '\x1b';
+  } else {
+
     return c;
+  }
+}
+
+void
+editorMoveCursor(int key){
+    switch(key){
+         case ARROW_UP:
+            if(E.cy != 0){
+                E.cy++;
+            }
+            break;
+        case ARROW_LEFT:
+            if(E.cx != 0){
+                E.cx--;
+            }
+            break;
+        case ARROW_DOWN:
+            if(E.cy != E.screenrows - 1){
+                E.cy--;
+            }
+            break;
+        case ARROW_RIGHT:
+            if(E.cx != E.screencols -1){
+                E.cx++;
+            }
+            break;
+    }
 }
 
 void 
 editorProcessKeypress(){
-    char c = editorReadKey();
+    int c = editorReadKey();
 
     switch (c) {
         case CTRL_KEY('q'):
-            write(STDIN_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[2J", 4);
             write(STDOUT_FILENO, "\x1b[H", 3);
             exit(0);
             break;
-        case 'w':
-            break;
-        case 'a':
-            break;
-        case 's':
+        case ARROW_UP:
+        case ARROW_LEFT:
+        case ARROW_DOWN:
+        case ARROW_RIGHT:
+            editorMoveCursor(c);
             break;
     }
 }
@@ -180,6 +230,7 @@ editorRefreshScreen() {
   abAppend(&ab, "\x1b[H", 3);
 
   editorDrawRows(&ab);
+
   char buf[32];
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy + 1, E.cx + 1);
   abAppend(&ab, buf, strlen(buf));
